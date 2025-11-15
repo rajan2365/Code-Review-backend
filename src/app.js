363f726ff974de3c8607986c0e3c8110cad2bc52
rajan2365 <rajantiwari2362005@gmail.com>
2025-11-15 18:22:37 +0530

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://code-review-ten-zeta.vercel.app/", 
   methods: ["GET", "POST"],
   credentials: true
 }));
